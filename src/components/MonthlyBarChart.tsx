524941cc8be09b68d1ac279fe18dd009a404cf2c
alejandrosur2-ux <alejandrosur2@gmail.com>
2025-09-02 02:23:52 -0600

    Create MonthlyBarChart.tsx

---
+++ src/components/MonthlyBarChart.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import {
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  CartesianGrid,
+} from 'recharts';
+
+type Props = {
+  data: { month: string; total: number }[];
+};
+
+export default function MonthlyBarChart({ data }: Props) {
+  return (
+    <div className="w-full h-80 bg-zinc-900/40 rounded-xl border border-zinc-800 p-4">
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart data={data} margin={{ top: 8, right: 16, left: 0, bottom: 8 }}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="month" />
+          <YAxis />
+          <Tooltip />
+          <Bar dataKey="total" />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
