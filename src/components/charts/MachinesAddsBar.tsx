5d4337e5c2dcd878960204b3206516cbd5d1bd91
alejandrosur2-ux <alejandrosur2@gmail.com>
2025-09-04 17:43:28 -0600

    Create MachinesAddsBar.tsx

---
+++ src/components/charts/MachinesAddsBar.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import {
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts";
+
+type Item = { mes: string; total: number };
+export default function MachinesAddsBar({ data }: { data: Item[] }) {
+  return (
+    <div className="h-56 w-full">
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart data={data}>
+          <XAxis dataKey="mes" />
+          <YAxis />
+          <Tooltip />
+          <Bar dataKey="total" fill="#3b82f6" />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
