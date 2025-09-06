59645aad714d7acfc328f56a931afc0ae2f4128e
alejandrosur2-ux <alejandrosur2@gmail.com>
2025-09-05 23:45:14 -0600

    Update server.ts

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -4,6 +4,5 @@ import { createClient } from "@supabase/supabase-js";
 export function supabaseServer() {
   const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
   const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-  // Cliente simple para consultas desde el server (RSC / rutas API)
   return createClient(url, key, { auth: { persistSession: false } });
 }
